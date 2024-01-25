d7e33809d5d4967017c4c8b75491110fb5158887
Sabrina Estévez <sesmo.dev@gmail.com>
2024-01-25 20:25:49 +0000

    añado div lila pero no limito que sea solo uno porque ya no doy pa más

--- src/js/component/Lights.jsx
+++ src/js/component/Lights.jsx
@@ -1,9 +1,50 @@
 import React from "react";
+import { useState } from "react";
 
 const Lights = () => {
+    const [lightOn, setLightOn] = useState(null);
+    const [purpleLight, setPurpleLight] = useState([]);
+
+    const handleLightClick = (light) => {
+        if (lightOn === light) {
+            setLightOn(null);
+        } else {
+            setLightOn(light);
+        }
+    };
+
+    const handlePurpleClick = () => {
+        const key = `purple-${purpleLight.length + 1}`;
+        setPurpleLight([...purpleLight, key]);
+    };
+
     return (
-        //Semáforo chachi pistachi
+        <div>
+            <div className="container">
+                <div
+                    onClick={() => handleLightClick('red')}
+                    className={`luzRoja ${lightOn === 'red' ? 'on' : 'off'}`}
+                ></div>
+                <div
+                    onClick={() => handleLightClick('amber')}
+                    className={`luzAmbar ${lightOn === 'amber' ? 'on' : 'off'}`}
+                ></div>
+                <div
+                    onClick={() => handleLightClick('green')}
+                    className={`luzVerde ${lightOn === 'green' ? 'on' : 'off'}`}
+                ></div>
+                {purpleLight.map((key) => (
+                    <div key={key} onClick={() => handleLightClick('purple')}
+                        className={`luzLila ${lightOn === 'purple' ? 'on' : 'off'}`}>
+
+                    </div>
+                ))}
+            </div>
+            <button onClick={handlePurpleClick}></button>
+
+        </div>
     );
 };
 
 export default Lights;
+
